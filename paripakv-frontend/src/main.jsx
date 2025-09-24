1e1e0faed8d1d58266a2caa1b3290557c45e84d2
chirdekaran262 <chirdekaran262@gmail.com>
2025-09-24 23:33:34 +0530

    solve react helment issue

--- paripakv-frontend/src/main.jsx
+++ paripakv-frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async';  // 👈 import HelmetProvider
 const rootElement = document.getElementById('root')
 rootElement.style.width = '100%'
 rootElement.style.maxWidth = 'none'
@@ -10,8 +9,6 @@ rootElement.style.padding = '0'
 
 createRoot(rootElement).render(
   <StrictMode>
-    <HelmetProvider>   {/* 👈 wrap your app */}
-      <App />
-    </HelmetProvider>
+    <App />
   </StrictMode>,
 )
